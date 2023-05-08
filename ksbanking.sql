3673beac2303c27b6c6ccd914cefe219e0740cd9
KaylaSSpruill <113389925+KaylaSSpruill@users.noreply.github.com>
2023-05-08 06:55:19 -0400

    Add files via upload

---
+++ ksbanking.sql
@@ -0,0 +1,8 @@
+SELECT * FROM sys.ksbankingusers;
+INSERT INTO ksbankingusers (username, password, pinum, balance)
+VALUES ('JimConway', 'ConwaytooCool', 2345, 1000.45);
+INSERT INTO ksbankingusers(username, password, pinnum, balance) 
+VALUES ('WillCarr', 'workingwill', 7956, 234.23);
+INSERT INTO ksbankingusers(username, password, pinnum, balance)
+VALUES ('KimKimmy', 'catslol', 2543, 2334.34);
+
